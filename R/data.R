edd96dd37f5f7fe153c99d52f50dc56313b1c634
Brianna Petrone <brianna.petrone@gmail.com>
2022-05-09 15:55:12 -0400

    Add example datasets for taxonomy and ASV assignment (inputs to asv_to_taxonomy)

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Example ASV assignments from assignSpecies_mod
+#'
+#' \describe{
+#' \item{asv}{Amplicon sequence variant (ASV) identified}
+#' \item{index}{The NCBI accession number of the sequence's exact match in the
+#' reference database}
+#' \item{label}{The taxonomic name associated with the NCBI accession number}
+#' }
+#'
+"assignment"
+
+#' Example taxonomy data structure
+#'
+"taxonomy"
