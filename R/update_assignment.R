ac0c3cab931a0db7fa026f694a85bfe1a884ff72
Brianna Petrone <brianna.petrone@gmail.com>
2022-05-09 15:58:46 -0400

    Framework for updating phyloseq taxonomic assignment

---
+++ R/update_assignment.R
@@ -0,0 +1,21 @@
+#' @title Update the taxonomic assignment of a phyloseq object
+#'
+#' @description Replaces the tax_table slot of a phyloseq object with updated
+#'   assignments based on a revised version of a reference database.
+#'
+#' @param ps Phyloseq object to be updated
+#' @param ref Path to the reference database (a FASTA file) to use for
+#'   assignment
+#'
+#' @import phyloseq
+#'
+#' @return An updated phyloseq object.
+#' @export
+#'
+#'
+
+update_assignment <- function(ps, ref){
+
+
+  ps
+}
