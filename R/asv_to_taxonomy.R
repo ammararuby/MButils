f60dc6d5312d749ded6f339a607b44dc49604762
Brianna Petrone <brianna.petrone@gmail.com>
2022-05-09 15:56:39 -0400

    Initial commit of functions to automate join to taxonomic assignment and last common ancestor procedure

---
+++ R/asv_to_taxonomy.R
@@ -0,0 +1,137 @@
+#' @title Join ASV assignments to taxonomy
+#'
+#' @description The output of the \code{assignSpecies_mod} function is a
+#'   two-column dataframe of ASVs and their taxonomic assignments based on exact
+#'   matching to reference sequences.  This joins the named taxonomic assignment
+#'   to a full taxonomy queried from NCBI, assuming that all assignments occur
+#'   to at least the species level, and potentially to subspecies levels
+#'   (currently hard-coded for subspecies, variety, and form).
+#'
+#' @param assignment The output of \code{assignSpecies_mod}
+#' @param taxonomy An \code{R6Class} object of class \code{Taxonomy} produced by
+#'   the \code{metacoder::taxonomy} function.
+#'
+#' @rawNamespace import(dplyr, except = id)
+#'
+#' @return A join of each ASV assignment to its full taxonomy, done from finer
+#'   taxonomic levels upward.
+#' @export
+#'
+
+asv_to_taxonomy = function(assignment, taxonomy){
+  # TODO: Check that taxonomy table has the right structure
+
+  # If present, remove '(nom. inval.)' notation, which interferes with joins
+  taxonomy <-
+    mutate(taxonomy,
+           across(.fns = ~gsub(pattern = ' \\(nom\\. inval\\.\\)$',
+                               replacement = '',
+                               .x)))
+
+  # The joins need to be done at sub-species level designations, which in rank
+  # order are:
+  # - subspecies
+  # - varietas
+  # - forma
+
+  #### Separate taxonomy table into these components ####
+  # Form
+  taxonomy.f <-
+    filter(taxonomy, !is.na(forma))
+  # Variety
+  taxonomy.var <-
+    filter(taxonomy, !is.na(varietas) & is.na(forma))
+  # Subspecies
+  taxonomy.ssp <-
+    filter(taxonomy, !is.na(subspecies) & is.na(varietas) & is.na(forma))
+  # Species
+  # For simplicity, just make a list of all species-level entries; this will
+  # ultimately be used in a right join so okay if there are some that won't be
+  # incorporated because they're assigned to a finer level
+  taxonomy.spp <-
+    taxonomy %>%
+    select(superkingdom:species) %>%
+    distinct()
+
+  #### Join each to name, accession, and ASV sequence from assignments ####
+  # Form
+  taxtab.f <-
+    assignment %>%
+    left_join(taxonomy.f, by = c('label' = 'forma')) %>%
+    filter(!is.na(species)) %>%
+    select(asv,
+           superkingdom,
+           kingdom,
+           phylum,
+           order,
+           family,
+           genus,
+           species,
+           subspecies,
+           varietas,
+           forma = label)
+  # Varietas
+  taxtab.var <-
+    assignment %>%
+    left_join(taxonomy.var, by = c('label' = 'varietas')) %>%
+    filter(!is.na(species)) %>%
+    select(asv,
+           superkingdom,
+           kingdom,
+           phylum,
+           order,
+           family,
+           genus,
+           species,
+           subspecies,
+           varietas = label,
+           forma)
+  # Subspecies
+  taxtab.ssp <-
+    assignment %>%
+    left_join(taxonomy.ssp, by = c('label' = 'subspecies')) %>%
+    filter(!is.na(species)) %>%
+    select(asv,
+           superkingdom,
+           kingdom,
+           phylum,
+           order,
+           family,
+           genus,
+           species,
+           subspecies = label,
+           varietas,
+           forma)
+  # Species
+  taxtab.spp <-
+    assignment %>%
+    filter(!(asv %in% c(taxtab.f$asv,
+                        taxtab.var$asv,
+                        taxtab.ssp$asv))) %>%
+    left_join(taxonomy.spp, by = c('label' = 'species')) %>%
+    select(asv,
+           superkingdom,
+           kingdom,
+           phylum,
+           order,
+           family,
+           genus,
+           species = label) %>%
+    # Fill missing columns with NA
+    mutate(subspecies = NA,
+           varietas = NA,
+           forma = NA) %>%
+    distinct()
+
+  #### Join all together ####
+  taxtab <-
+    bind_rows(taxtab.spp,
+              taxtab.ssp,
+              taxtab.var,
+              taxtab.f)
+
+  taxtab
+
+  # TODO: Some check of completeness? Flag any rows that are suspicious
+  # for a bad join?
+}
---
+++ R/lca.R
@@ -0,0 +1,41 @@
+#' @title Find the last common ancestor of assignSpecies taxonomic assignments
+#'
+#' @description The output of the \code{assignSpecies_mod} function is a
+#'   two-column dataframe of ASVs and their taxonomic assignments based on exact
+#'   matching to reference sequences: if multiple sequences are matched, the ASV
+#'   will be repeated across rows, one per match.  This function collapses each
+#'   ASV to a single row labeled with the last common ancestor of all its
+#'   matched taxa (*e.g.*, an ASV matching to an ASV matching to both wheat
+#'   [Triticum aestivum] and rye [Secale cereale] was relabeled as Poaceae, the
+#'   family shared by both genera).
+#'
+#' @param assignment The output of \code{assignSpecies_mod} (*i.e.* matches of
+#'   ASVs to sequences in a metabarcoding reference database).
+#'
+#' @return A taxonomy table with taxonomic assignment per ASV, compatible for
+#'   use in a phyloseq object.
+#' @export
+#'
+
+lca <- function(assignment){
+  # Group assignments by ASV
+  lca <-
+    assignment %>%
+    group_by(asv) %>%
+    summarize_all(n_distinct,
+                  na.rm = TRUE) %>%
+    column_to_rownames(var = 'asv')
+
+  # Now, relabel all those with >1 name at a particular level as NA
+  # As a placeholder, keep only the first species, knowing it will be overwritten
+  assignment.lca <-
+    assignment %>%
+    group_by(asv) %>%
+    summarize_all(first) %>%
+    column_to_rownames(var = 'asv')
+
+  # Relabel
+  assignment.lca[lca > 1] = NA
+
+  assignment.lca
+}
