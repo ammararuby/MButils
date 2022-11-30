302a4db358cb58e2147f0f17e15e2db4278280b0
Brianna Petrone <brianna.petrone@gmail.com>
2022-11-30 14:31:36 -0500

    Update documentation for biplot function

--- R/global.R
+++ R/global.R
@@ -15,8 +15,21 @@
   utils::globalVariables("varietas")
   utils::globalVariables("forma")
   utils::globalVariables("label")
+  utils::globalVariables("taxon_id")
+  utils::globalVariables("index")
+  utils::globalVariables(".")
+  # biplot
+  utils::globalVariables("X1")
+  utils::globalVariables("X2")
+  utils::globalVariables("PC1")
+  utils::globalVariables("PC2")
+  utils::globalVariables("slope")
+  utils::globalVariables("ang")
+  utils::globalVariables("variable")
   # lca
   utils::globalVariables("asv")
+  # lookup_tax_data
+  utils::globalVariables("correct_taxon_names")
   # merge_into_phyloseq
   utils::globalVariables("row_names")
 
