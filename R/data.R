921602632ed81baf4225240a5442ddabcbdecc00
Brianna Petrone <brianna.petrone@gmail.com>
2023-01-25 14:12:23 -0500

    Add documentation for seqtab data

--- R/data.R
+++ R/data.R
@@ -12,3 +12,17 @@
 #' Example taxonomy data structure
 #'
 "taxonomy"
+
+#' Example sequence table
+#'
+#' Rows are samples (named here by their plate and well location), and columns
+#' are *trnL* amplicon sequence variants (ASVs). Values indicate counts of an
+#' ASV detected in a given sample inferred by DADA2.
+#'
+#' @format ## `seqtab` A data frame with 52 rows and 110 columns:
+#' \describe{
+#'   \item{ATCACGTTTTCCGAAAACAAACAACGGTTCAGAAAGCGAAAATCAAAAAG}{ASV1}
+#'   \item{ATCCCTTTTTTGAAAAACAAGTGGTTCTCAAACTAGAACCCAAAGGAAAAG}{ASV2}
+#'   ...
+#' }
+"seqtab"
